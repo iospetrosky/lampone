64b1f19c1291305352afbb36fbcbe3c7a9ccb279
iospetrosky <lorenzo.pedrotti@gmail.com>
2022-10-29 10:51:09 +0100

    works

--- moisture03.py
+++ moisture03.py
@@ -48,7 +48,7 @@ dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
 moist_pins = [8,16]
 light_pin = 29
-relay_pins = [7,11,15] # of the moist sensors, the last is the light switch
+relay_pins = [7,11] # of the moist sensors, the last is the light switch
 pots = ['Strawberry','Ivy']
 
 # returns 0 when the moisture led is on meaning that the soil
@@ -62,22 +62,17 @@ a_subject = "State of the sensors at - {}" .format(dt_string)
 a_message = ""
 
 #reset all the relay ports, this should also switch off the light
-for x in range(0,3):
+for x in range(0,2):
     GPIO.output(relay_pins[x], GPIO.HIGH)
 
+#this will switch on all the moisture sensors
+print("Activation of relay at pin {}".format(relay_pins[0]))
+# switches are triggered with LOW!!
+GPIO.output(relay_pins[0], GPIO.LOW)
+time.sleep(5) #wait for the sensor to sense something
+
 for pin in range(0,2): #only the one of the moist sensors
-    print("Activation of relay at pin {}".format(relay_pins[pin]))
-    # switches are triggered with LOW!!
-    GPIO.output(relay_pins[pin], GPIO.LOW)
-
-    time.sleep(5) #wait for the sensor to sense something
-
     a_message += "Test sequence for {}: ".format(pin)
-    #ms = 1000 #dumb value
-    #for x in range(1,75):
-    #    ms = GPIO.input(moist_pin)
-    #    a_message += "{}".format(ms)
-    #    time.sleep(0.1)
 
     ms = GPIO.input(moist_pins[pin])
     a_message += "{}".format(ms)
@@ -89,16 +84,15 @@ for pin in range(0,2): #only the one of the moist sensors
         a_message += "{} need water\n" .format(pots[pin])
     else:
         a_message += "{} is wet enough\n" .format(pots[pin])
-        
-    GPIO.output(relay_pins[pin], GPIO.HIGH)
-    time.sleep(1)
-    
 
+#turn off the sensors and avoid electrolisys        
+GPIO.output(relay_pins[0], GPIO.HIGH)
+time.sleep(1)
 
 #now check the light sensor
 light_level = rc_time(light_pin)
 a_message += "\nLight level is: {}".format(light_level)
-if light_level > 300: # higher means darker
+if light_level > 400: # higher means darker
     if datetime.now().hour > 8 and datetime.now().hour < 18:
         ## only during the day
         GPIO.output(relay_pins[-1] , GPIO.LOW)
